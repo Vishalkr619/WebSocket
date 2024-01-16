39c334cbf38b9f483ea5d2b0c9bd31fb50aa8497
vishalkr619 <bitugpt619@gmail.com>
2024-01-16 11:24:28 +0530

    space add
    space add

--- src/main/java/com/chat/RealTimeCollaborativeApplication.java
+++ src/main/java/com/chat/RealTimeCollaborativeApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RealTimeCollaborativeApplication {
 
+	
+
 	public static void main(String[] args) {
 		SpringApplication.run(RealTimeCollaborativeApplication.class, args);
 	}
